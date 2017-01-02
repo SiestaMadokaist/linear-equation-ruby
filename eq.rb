5e31451293c89209786d04b9648f46f18ac592e1
Rama Patria Himawan <siestamadokaist@gmail.com>
2017-01-02 19:08:34 +0700

    easier example

--- eq.rb
+++ eq.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../linear_equation', __FILE__)
 
 le = LinearEquation.new do
-  x - y + z = 10
-  3.x - 3.y + 2.z = 11
-  2.x + 3.z = 9
+  x - 2.y + z = 9
+  2.x - y = 4
+  y + z = 12
 end
 
 puts(le.solution)
